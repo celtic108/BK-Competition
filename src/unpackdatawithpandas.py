b40c11cf14061de809dfd85a537389d2fcf24fba
Greg Treleaven <gdtr11@gmail.com>
2016-09-04 06:39:02 -0500

    Added random forest classifier attempt 2

--- src/unpackdatawithpandas.py
+++ src/unpackdatawithpandas.py
@@ -1,10 +1,30 @@
 import pandas as pd
 import numpy as np
+import pylab as P
+from sklearn.ensemble import RandomForestClassifier
 
-df = pd.read_csv('../Data/train_numeric.csv', header = 0, nrows=9999)
-#df = pd.read_csv('../Data/train_numeric.csv', header = 0, skiprows=10000, nrows=9999)
-print type(df)
-#df.types
-print df.info()
-print df.describe()
-print df.head(3)
+df = pd.read_csv('../Data/train_numeric.csv', header = 0, nrows=99999)
+test = pd.read_csv('../Data/train_numeric.csv', header = 0, skiprows=100000, nrows=99999)
+#print type(df)
+#df.types --- causes error
+#print df.info()
+print df.describe()  #Displays summary data about columns
+#print df.head(3)
+df.fillna(-9, inplace = True)
+#print df.head(3)
+test.fillna(-9, inplace = True)
+
+#df['L0_S0_F4'].hist() #this shows a definite bimodal distribution
+#P.show()
+
+
+traindata = df.values
+testdata = test.values
+forest = RandomForestClassifier(n_estimators = 968)
+forest = forest.fit(traindata[0::,0:968], traindata[0::,969])
+score = forest.score(testdata[0::,0:968], testdata[0::,969])
+output = forest.predict(testdata[0::,0:968])
+print score
+print type(output)
+np.savetxt("../output files/fake_trial.csv", output, delimiter=",")
+#output.to_csv('../output files/fake_trial.csv, sep=',')
